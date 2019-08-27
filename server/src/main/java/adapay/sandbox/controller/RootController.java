aa6d2814af35fce18908728ccce3fd898b251c06
bit-ranger <sincerebravefight@gmail.com>
2019-08-27 16:10:31 +0800

    20190827, v1.0.0, properties

--- server/src/main/java/adapay/sandbox/controller/RootController.java
+++ server/src/main/java/adapay/sandbox/controller/RootController.java
@@ -1,7 +1,5 @@
 package adapay.sandbox.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,13 +12,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class RootController {
 
-    @Autowired
-    private BuildProperties buildProperties;
 
     @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
-    public BuildProperties index() {
-        return buildProperties;
+    public String index() {
+        return "hello sandbox";
     }
 
 }
